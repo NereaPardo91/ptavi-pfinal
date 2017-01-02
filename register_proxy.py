76c18116168d84682a39b34bbc407a75db9a8f43
NereaPardo91 <n.pardonavarro1991@gmail.com>
2017-01-02 23:27:44 +0100

    Depurando un poco...

--- register_proxy.py
+++ register_proxy.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-"""
+'''
 Clase (y programa principal) para un servidor de eco en UDP simple
-"""
+'''
 
 import socketserver 
 import sys
@@ -14,34 +14,34 @@ from xml.sax.handler import ContentHandler
 try:
     CONFIG = sys.argv[1]
 except:
-    sys.exit("Usage: python3 proxy_registrar.py config")
+    sys.exit('Usage: python3 proxy_registrar.py config')
 
 
 class ReadFich(ContentHandler):
 
     def __init__(self):
-        self.server_name = ""
-        self.server_ip = ""
-        self.server_puerto = ""
-        self.database_path = ""
-        self.database_passwdpath = ""
-        self.log_path = ""
+        self.server_name = ''
+        self.server_ip = ''
+        self.server_puerto = ''
+        self.database_path = ''
+        self.database_passwdpath = ''
+        self.log_path = ''
         self.Datos = []
 
     def startElement(self, name, attrs):
         if name == 'server':
-            self.server_name = attrs.get('name', "")
-            self.server_ip = attrs.get('ip', "")
-            self.server_puerto = attrs.get('puerto', "")
+            self.server_name = attrs.get('name', '')
+            self.server_ip = attrs.get('ip', '')
+            self.server_puerto = attrs.get('puerto', '')
             atrib = {'name': self.server_name, 'ip': self.server_ip, 'puerto': self.server_puerto}
             self.Datos.append(atrib)
         elif name == 'database':
-            self.database_path = attrs.get('path', "")
-            self.database_passwdpath = attrs.get('passwdpath', "")
+            self.database_path = attrs.get('path', '')
+            self.database_passwdpath = attrs.get('passwdpath', '')
             atrib = {'path' : self.database_path, 'passwdpath': self.database_passwdpath}
             self.Datos.append(atrib)
         elif name == 'log':
-            self.log_path = attrs.get('path', "")
+            self.log_path = attrs.get('path', '')
             atrib = {'path': self.log_path}
             self.Datos.append(atrib)
 
@@ -49,29 +49,26 @@ class ReadFich(ContentHandler):
         return self.Datos
 
 class SIPRegisterHandler(socketserver.DatagramRequestHandler):
-
-    """
+    '''
     SIPRegister server class
-    """
+    '''
     list_users = {}
 
     def handle(self):
-
-        """
+        '''
         Registro
-        """
+        '''
         Linea = self.rfile.read()
         Line = Linea.decode('utf-8').split()
         print(Linea.decode('utf-8'))
-        todoOk =""
+        Estado = ''
         if Line[0] == 'REGISTER':
             for i in Line:
-                if i == "Authorization:":
-                    todoOk = "todo ok"
-                    self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
-                    print(todoOk)
-            if(todoOk != "todo ok"):
-                self.wfile.write(b"SIP/2.0 401 Unauthorized\r\n\r\n")
+                if i == 'Authorization:':
+                    Estado = 'Autorizado'
+                    self.wfile.write(b'SIP/2.0 200 OK\r\n\r\n')
+            if(Estado != 'Autorizado'):
+                self.wfile.write(b'SIP/2.0 401 Unauthorized\r\n\r\n')
 
 parser = make_parser()
 sHandler = ReadFich()
@@ -91,10 +88,10 @@ Log_Path = Datos[2]['path']
     #my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     #my_socket.connect((str(IP_RegProxy), int(Puerto_RegProxy)))
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     serv = socketserver.UDPServer(('', 7777), SIPRegisterHandler)
-    print("Lanzando servidor UDP de eco...")
+    print('Lanzando servidor UDP de eco...')
     try:
         serv.serve_forever()
     except KeyboardInterrupt:
-        print("Finalizado servidor")
+        print('Finalizado servidor')
