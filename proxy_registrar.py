5d1eb6792d33fa3273904676fed291c8e11d380e
NereaPardo91 <n.pardonavarro1991@gmail.com>
2017-01-11 20:26:10 +0100

    PEP8 PASADO

--- proxy_registrar.py
+++ proxy_registrar.py
@@ -4,19 +4,21 @@
 PRACTICA FINAL PTAVI, NEREA PARDO NAVARRO, 11-01-17
 '''
 
-import socketserver 
+import socketserver
 import socket
 import sys
 import time
 import json
+import hashlib
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
-import hashlib
 
-try:
-    CONFIG = sys.argv[1]
-except:
-    sys.exit('Usage: python3 proxy_registrar.py config')
+
+def Content_Log(Path_Log, Contenido_Log):
+    Time_Log = time.time()
+    Fichero_Log = open(Path_Log, 'a+')
+    Fichero_Log.write(Contenido_Log)
+    Fichero_Log.close()
 
 
 class ReadFich(ContentHandler):
@@ -40,7 +42,7 @@ class ReadFich(ContentHandler):
         elif name == 'database':
             self.database_path = attrs.get('path', '')
             self.database_passwdpath = attrs.get('passwdpath', '')
-            atrib = {'path' : self.database_path, 'passwdpath': self.database_passwdpath}
+            atrib = {'path': self.database_path, 'passwdpath': self.database_passwdpath}
             self.Datos.append(atrib)
         elif name == 'log':
             self.log_path = attrs.get('path', '')
@@ -49,7 +51,12 @@ class ReadFich(ContentHandler):
 
     def get_tags(self):
         return self.Datos
-        
+
+try:
+    CONFIG = sys.argv[1]
+except:
+    sys.exit('Usage: python3 proxy_registrar.py config')
+
 parser = make_parser()
 sHandler = ReadFich()
 parser.setContentHandler(sHandler)
@@ -61,7 +68,14 @@ IP_Proxy = Datos[0]['ip']
 Puerto_Proxy = Datos[0]['puerto']
 Path_Database = Datos[1]['path']
 Passwdpath_Database = Datos[1]['passwdpath']
-Log_Path = Datos[2]['path']
+Path_Log = Datos[2]['path']
+
+Hora_Log = time.time()
+Hora_Log = time.strftime('%Y%m%d%H%M%S', time.gmtime(Hora_Log))
+
+Contenido_Log = Hora_Log + ' Starting...\r\n'
+Content_Log(Path_Log, Contenido_Log)
+
 
 class SIPRegisterHandler(socketserver.DatagramRequestHandler):
 
@@ -80,10 +94,13 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
         print('Recibido de Cliente:')
         print(Linea)
         Direccion_IP = self.client_address[0]
-        IP = str(Direccion_IP) 
+        Puerto_Cliente = self.client_address[1]
+        IP = str(Direccion_IP)
         Direccion_SIP = Line[1].split(':')
         SIP_Fich_Password = Direccion_SIP[1]
         Direccion_SIP = Direccion_SIP[0] + ':' + Direccion_SIP[1]
+        Contenido_Log = '\n' + Hora_Log + ' Received from ' + str(Direccion_IP) + ':' + str(Puerto_Cliente) + ' ' + Linea + '\r\n'
+        Content_Log(Path_Log, Contenido_Log)
         Estado = ''
         REGISTRADO = 0
         nonce = '1234123412341234'
@@ -103,15 +120,18 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
                             Password_Fich = Password_Fich.split('\n')[0]
                             Procc_Autentic = nonce + Password_Fich
                             Pass_Autentic = hashlib.sha1()
-                            Pass_Autentic.update(bytes(Procc_Autentic,'utf-8'))
+                            Pass_Autentic.update(bytes(Procc_Autentic, 'utf-8'))
                             Procc_Autentic = Pass_Autentic.digest()
                             Procc_Autentic_Client = Line[8].split('=')[1]
                     if str(Procc_Autentic) == Procc_Autentic_Client:
                         Autentificacion = 1
-                        #print("Contraseñas iguales\n")
+                        print("Contraseñas iguales\n")
                     else:
-                        #print("Contraseña incorrecta\n")
-                        self.wfile.write(b'SIP/2.0 401 Unauthorized\r\n\r\n')
+                        print("Contraseña incorrecta\n")
+                        self.wfile.write(b'SIP/2.0 401 Unauthorized\r\n\r')
+                        Contenido_Log = Hora_Log + ' Send to ' + str(Direccion_IP) + ':' + str(Puerto_Cliente) + ' ' + 'SIP/2.0 401 Unauthorized' + '\r\n'
+                        Content_Log(Path_Log, Contenido_Log)
+
                     if Autentificacion == 1:
                         Puerto_Client = Line[1].split(':')[2]
                         Expires = Line[4]
@@ -120,11 +140,17 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
                         Hora = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(Hora_Expiracion))
                         Estado = 'Autorizado'
                         self.wfile.write(b'SIP/2.0 200 OK\r\n\r\n')
+                        Contenido_Log = Hora_Log + ' Send to ' + str(Direccion_IP) + ':' + str(Puerto_Cliente) + ' ' + 'SIP/2.0 200 OK' + '\r\n'
+                        Content_Log(Path_Log, Contenido_Log)
                         self.list_users[Direccion_SIP] = [IP, Hora, Puerto_Client, Expires]
                         fichjson = self.register2json()
+                        if Expires == 0:
+                            del self.list_users[Direccion_SIP]
 
             if(Estado != 'Autorizado'):
                 self.wfile.write(b'SIP/2.0 401 Unauthorized\r\n\r\nWWW Authenticate: Digest nonce=' + bytes(nonce, 'utf-8'))
+                Contenido_Log = Hora_Log + ' Send to ' + str(Direccion_IP) + ':' + str(Puerto_Cliente) + ' ' + 'SIP/2.0 401 Unauthorized WWW Authenticate: Digest nonce=' + nonce
+                Content_Log(Path_Log, Contenido_Log)
 
         elif Line[0] == 'INVITE':
             for i in self.list_users.keys():
@@ -134,14 +160,22 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
                     my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
                     my_socket.connect((('127.0.0.1'), int(valores[2])))
                     my_socket.send(bytes(Linea, 'utf-8') + b'\r\n\r\n')
+                    Contenido_Log = Hora_Log + ' Send to ' + str(valores[0]) + ':' + str(valores[2]) + ' ' + Linea + '\r\n'
+                    Content_Log(Path_Log, Contenido_Log)
                     print('Enviando a Server...')
                     print(Linea)
                     data = my_socket.recv(1024)
                     print('Respuesta Server... ', data.decode('utf-8'))
+                    Contenido_Log = Hora_Log + ' Received ' + str(valores[0]) + ':' + str(valores[2]) + ' ' + Linea
+                    Content_Log(Path_Log, Contenido_Log)
                     self.wfile.write(data)
+                    Contenido_Log = Hora_Log + ' Send to ' + str(Direccion_IP) + ':' + str(Puerto_Cliente) + ' ' + data.decode('utf-8')
+                    Content_Log(Path_Log, Contenido_Log)
                     REGISTRADO = 1
             if REGISTRADO == 0:
                 self.wfile.write(b'SIP/2.0 404 User Not Found')
+                Contenido_Log = Hora_Log + ' Send to ' + str(Direccion_IP) + ':' + str(Puerto_Cliente) + ' ' + 'SIP/2.0 404 User Not Found'
+                Content_Log(Path_Log, Contenido_Log)
 
         elif Line[0] == 'ACK':
             valores = self.list_users[Direccion_SIP]
@@ -149,11 +183,19 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
             my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             my_socket.connect((('127.0.0.1'), int(valores[2])))
             my_socket.send(bytes(Linea, 'utf-8') + b'\r\n\r\n')
+
+            Contenido_Log = Hora_Log + ' Send to ' + str(valores[0]) + ':' + str(valores[2]) + ' ' + Linea + '\r\n'
+            Content_Log(Path_Log, Contenido_Log)
+
             print('Enviando a Server...')
             print(Linea)
             data = my_socket.recv(1024)
             print('Respuesta Server... ', data.decode('utf-8'))
+            Contenido_Log = Hora_Log + ' Received ' + str(valores[0]) + ':' + str(valores[2]) + ' ' + data.decode('utf-8')
+            Content_Log(Path_Log, Contenido_Log)
             self.wfile.write(data)
+            Contenido_Log = Hora_Log + ' Send to ' + str(Direccion_IP) + ':' + str(Puerto_Cliente) + ' ' + data.decode('utf-8')
+            Content_Log(Path_Log, Contenido_Log)
 
         elif Line[0] == 'BYE':
             valores = self.list_users[Direccion_SIP]
@@ -161,12 +203,17 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
             my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             my_socket.connect((('127.0.0.1'), int(valores[2])))
             my_socket.send(bytes(Linea, 'utf-8') + b'\r\n\r\n')
+            Contenido_Log = Hora_Log + ' Send to ' + str(valores[0]) + ':' + str(valores[2]) + ' ' + Linea + '\r\n'
+            Content_Log(Path_Log, Contenido_Log)
             print('Enviando a Server...')
             print(Linea)
             data = my_socket.recv(1024)
+            Contenido_Log = Hora_Log + ' Received ' + str(valores[0]) + ':' + str(valores[2]) + ' ' + data.decode('utf-8')
+            Content_Log(Path_Log, Contenido_Log)
             print('Respuesta Server... ', data.decode('utf-8'))
             self.wfile.write(data)
-
+            Contenido_Log = Hora_Log + ' Send to ' + str(Direccion_IP) + ':' + str(Puerto_Cliente) + ' ' + data.decode('utf-8')
+            Content_Log(Path_Log, Contenido_Log)
 
     def register2json(self):
         """
@@ -192,3 +239,5 @@ if __name__ == '__main__':
         serv.serve_forever()
     except KeyboardInterrupt:
         print('Finalizado servidor')
+        Contenido_Log = Hora_Log + ' Finishing...'
+        Content_Log(Path_Log, Contenido_Log)
